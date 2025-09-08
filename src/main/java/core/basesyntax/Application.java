7114985dbcbbe31e2228fb4fd86e655e8b3a8fbc
Oleksandr Hordienko <code57231@gmail.com>
2025-09-08 23:29:11 +0300

    create a method for random Ball

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -1,12 +1,17 @@
 package core.basesyntax;
+
 public class Application {
+
     public static void main(String[] args) {
-       Ball firstBall = Lottery.getRandomBall();
-       Ball secondBall = Lottery.getRandomBall();
-       Ball thirdBall = Lottery.getRandomBall();
-        System.out.println("the ball color is " + firstBall.color + "\n" + "the ball number is " + firstBall.number);
-        System.out.println("the ball color is " + secondBall.color + "\n" + "the ball number is " + secondBall.number);
-        System.out.println("the ball color is " + thirdBall.color + "\n" + "the ball number is " + thirdBall.number);
+        Ball firstBall = Lottery.getRandomBall();
+        Ball secondBall = Lottery.getRandomBall();
+        Ball thirdBall = Lottery.getRandomBall();
+        System.out.println("the ball color is " + firstBall.getColor()
+                + "\n" + "the ball number is " + firstBall.getNumber());
+        System.out.println("the ball color is " + secondBall.getColor()
+                + "\n" + "the ball number is " + secondBall.getNumber());
+        System.out.println("the ball color is " + thirdBall.getColor()
+                + "\n" + "the ball number is " + thirdBall.getNumber());
         // create three balls using class Lottery and print information about them in console
     }
 }
